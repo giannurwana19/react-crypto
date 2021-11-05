273feb051adad561ae59262be524dcf26f25cbdf
giannurwana19 <giannurwana19@gmail.com>
2021-11-05 07:15:59 +0700

    search text coins in cryptocurrencies

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,18 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useGetCryptosQuery } from '../services/cryptoApi';
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Input } from 'antd';
 import { Link } from 'react-router-dom';
 import millify from 'millify';
 
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(data?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const filteredData = data?.data?.coins.filter(coin =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filteredData);
+  }, [data, searchTerm]);
 
   if (isFetching) return 'Loading...';
 
   return (
     <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={e => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map(currency => (
           <Col
