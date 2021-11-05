70745caba5545967f128073380071c196044c5c1
giannurwana19 <giannurwana19@gmail.com>
2021-11-05 13:41:35 +0700

    responsive navbar in mobile device

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -38,8 +38,6 @@ const CryptoDetails = () => {
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
-  console.log(timePeriod);
-
   const stats = [
     {
       title: 'Price to USD',
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,38 @@
-import { Avatar, Menu, Typography } from 'antd';
+import { Avatar, Button, Menu, Typography } from 'antd';
 import { Link } from 'react-router-dom';
 import icon from '../images/cryptocurrency.png';
 import {
   BulbOutlined,
   FundOutlined,
   HomeOutlined,
+  MenuOutlined,
   MoneyCollectOutlined,
 } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -16,21 +40,28 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}>
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark">
-        <Menu.Item key="home" icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="cryptocurrencies" icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item key="exchanges" icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
-        <Menu.Item key="news" icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
+      {activeMenu && (
+        <Menu theme="dark">
+          <Menu.Item key="home" icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="cryptocurrencies" icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item key="exchanges" icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item key="news" icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   );
 };
